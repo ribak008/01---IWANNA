a485aeb063a6f90092a886527a2b28aae09adf8a
Kabir Ravest <ka.ravest@duocuc.cl>
2025-04-15 20:57:00 -0400

    formulario cotizacion v1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,10 @@ import { Stack } from 'expo-router/stack';
 
 export default function Layout() {
   return (
-    <Stack>
+    <Stack screenOptions={{
+      headerShown: false,
+    }}>
+
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="Account" options={{ headerShown: false }} />
       <Stack.Screen name="Favoritos" options={{ headerShown: false }} />
