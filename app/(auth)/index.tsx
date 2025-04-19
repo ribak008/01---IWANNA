6fb31187968c0f82ce197063d916674e96cd8c3d
Kabir Ravest <ka.ravest@duocuc.cl>
2025-04-19 13:08:00 -0400

    color fondo login

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -75,6 +75,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         padding: 20,
+        backgroundColor: 'white',
     },
     input: {
         width: '100%',
